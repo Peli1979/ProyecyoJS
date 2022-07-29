8dcd899ef35ffdc3a9b126ce07f4cf8daaddbadd
Martin Rozada <martinrozada@gmail.com>
2022-07-28 21:35:50 -0300

    2do commit

--- js/simulador.js
+++ js/simulador.js
@@ -1,24 +1,45 @@
 
  
+ function reserva(){
+    let precioHoraDia = 2000 
+    let preciohoraNoche=3000
 
-let precioHoraDia = 2000 
-let preciohoraNoche=3000
-
-let costo=0
+    let costo=0
 
 
-let diaReserva = prompt("elige un dia: LUNES/MARTES/MIERCOLES/JUEVES/VIERNES")
+    let diaReserva = prompt("elige un dia: LUNES/MARTES/MIERCOLES/JUEVES/VIERNES")
 
-let horarioReserva =prompt("elige un horario de 17 a 23hs: ")
+    let horarioReserva =prompt("elige un horario de 17 a 23hs: ")
 
-let ingresarNombre = prompt("Ingresar nombre: ")
+    let ingresarNombre = prompt("Ingresar nombre: ")
 
-let cantidad= +prompt("ingrese cantidad de horas a jugar: ")
+    let cantidad= +prompt("ingrese cantidad de horas a jugar: ")
 
-if(horarioReserva<=20){
+    if(horarioReserva<=20){
     costo= precioHoraDia*cantidad
-}else{costo=preciohoraNoche*cantidad}
+    }else{costo=preciohoraNoche*cantidad}
     
-alert(" Turno  DE dia "+diaReserva+" "+horarioReserva+"HS Nombre: "+ingresarNombre+ ", TOTAL A PAGAR: "+costo+ "pesos" )
+    alert("Turno  DE dia "+diaReserva+" "+horarioReserva+"HS Nombre: "+ingresarNombre+ ", TOTAL A PAGAR: "+costo+ "pesos" )
+}
+
+reserva()
+while (nuevaReserva=prompt("Quieres hacer una nueva reserva: ")){
+    if(nuevaReserva=="si"){reserva()}
+    else{exit}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
