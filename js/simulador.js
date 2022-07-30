234017ab56a8d70957bb25ec93934b3c0ef41bc4
Martin Rozada <martinrozada@gmail.com>
2022-07-29 23:21:37 -0300

    simulador final

--- js/simulador.js
+++ js/simulador.js
@@ -9,22 +9,47 @@
 
     let diaReserva = prompt("elige un dia: LUNES/MARTES/MIERCOLES/JUEVES/VIERNES")
 
-    let horarioReserva =prompt("elige un horario de 17 a 23hs: ")
+    if((diaReserva=="lunes") || (diaReserva=="LUNES"))  {
+        alert("Elegiste lunes")}
+    else if((diaReserva=="martes")||(diaReserva=="MARTES") )
+        alert("elegiste martes")
+    else if((diaReserva=="miercoles")||(diaReserva=="MIERCOLES") )
+        alert("elegiste miercoles")
+    else if((diaReserva=="jueves")||(diaReserva=="JUEVES") )
+        alert("elegiste jueves")
+    else if((diaReserva=="viernes")||(diaReserva=="VIERNES"))
+        alert("elegiste viernes")
+    else{alert("no es un dia de la semana")+exit}
+   
+    
+    let horarioReserva =+prompt("elige un horario de 17 a 23hs: ")
+
+    if((horarioReserva<17) || (horarioReserva>23)){alert("No es un horario posible")+exit}
+
+   
 
     let ingresarNombre = prompt("Ingresar nombre: ")
 
     let cantidad= +prompt("ingrese cantidad de horas a jugar: ")
+    while((horarioReserva+cantidad)>24){
+        alert("No es posible reservar, el predio cierra a las 24hs")
+        cantidad= +prompt("ingrese cantidad de horas a jugar: ")
+    }
+    let hasta=horarioReserva+cantidad
 
     if(horarioReserva<=20){
     costo= precioHoraDia*cantidad
     }else{costo=preciohoraNoche*cantidad}
+
     
-    alert("Turno  DE dia "+diaReserva+" "+horarioReserva+"HS Nombre: "+ingresarNombre+ ", TOTAL A PAGAR: "+costo+ "pesos" )
-}
+    
+    alert("Nombre: " +ingresarNombre+"\nTurno dia: "+diaReserva+ "\nHorario: "+horarioReserva+"HS \nHasta: " +hasta+"hs\nTOTAL A PAGAR: "+costo+"pesos") 
+}            
 
 reserva()
+
 while (nuevaReserva=prompt("Quieres hacer una nueva reserva: ")){
-    if(nuevaReserva=="si"){reserva()}
+    if((nuevaReserva=="si")|| (nuevaReserva=="SI")){reserva()}
     else{exit}
 }
 
